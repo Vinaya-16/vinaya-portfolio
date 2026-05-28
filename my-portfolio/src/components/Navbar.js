4f0d3b92f266c74c17a320fde0d08ade62dd8e20
Vinaya-16 <vinayapatole16@gmail.com>
2026-05-28 12:57:30 +0530

    Created Hamburger for mobile view

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Navbar () {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <div className='navbar-section'>
-            {/* <div className='navbar-logo'> */}
                 <a href='#Home' className='navbar-logo'>&lt;VP/&gt;</a>
-            {/* </div> */}
-                <ul className='navlinks'>
+
+                <div className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                    <span className='bar'></span>
+                    <span className='bar'></span>
+                    <span className='bar'></span>
+                </div>
+
+                <ul className={`navlinks ${isOpen ? 'active' : ''}`}>
                 <li className='navlink'><a href="#About">About</a></li>
                 <li className='navlink'><a href="#Skills">Skills</a></li>
                 <li className='navlink'><a href="#Projects">Projects</a></li>
