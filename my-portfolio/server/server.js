35885f9fce5c5464de8d7fc08e96f96abca04e57
Vinaya-16 <vinayapatole16@gmail.com>
2026-05-29 22:40:55 +0530

    MongoDB Connected

--- my-portfolio/server/server.js
+++ my-portfolio/server/server.js
@@ -1,57 +1,50 @@
 import express from 'express';
 import cors from 'cors';
-import fs from 'fs';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import Message from './models/Message.js';
+
+dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/contact', (req, res) => {
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
 
-    const newMsg = req.body;
+app.post('/contact', async (req, res) => {
 
-    fs.readFile('messages.json', 'utf-8', (err, data) => {
+  try {
 
-        let messages = [];
+    const newMessage = new Message(req.body);
 
-        if (!err && data) {
-            messages = JSON.parse(data);
-        }
+    await newMessage.save();
 
-        messages.push(newMsg);
-
-        fs.writeFile(
-            'messages.json',
-            JSON.stringify(messages, null, 2),
-            (err) => {
-                if (err) {
-                    return res.status(500).json({
-                        message: 'Error saving message'
-                    });
-                }
-
-                res.json({
-                    message: 'Message saved successfully'
-                })
-            }
-        );
+    res.json({
+      message: 'Message saved successfully'
     });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: 'Error saving message'
+    });
+  }
 });
 
-app.get('/messages', (req, res) => {
+app.get('/messages', async (req, res) => {
 
-  fs.readFile('messages.json', 'utf-8', (err, data) => {
+  const messages = await Message.find();
 
-    if (err) {
-      return res.json([]);
-    }
-
-    res.send(JSON.parse(data));
-  });
+  res.json(messages);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
